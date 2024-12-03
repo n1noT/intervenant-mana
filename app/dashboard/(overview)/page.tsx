58d29e75227f6ee1eda329733d99960598e6f618
n1noT <nino.toutain@etu.unilim.fr>
2024-12-03 10:59:10 +0100

    quick clean

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,6 @@
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
-import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
-import CardWrapper from '@/app/ui/dashboard/cards';
  
 export default async function Page() {
   return (
--- app/dashboard/intervenants/[id]/edit/not-found.tsx
+++ app/dashboard/intervenants/[id]/edit/not-found.tsx
@@ -8,8 +8,8 @@ export default function NotFound() {
       <h2 className="text-xl font-semibold">404 Not Found</h2>
       <p>Could not find the requested invoice.</p>
       <Link
-        href="/dashboard/invoices"
-        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        href="/dashboard/intervenants"
+        className="mt-4 rounded-md bg-purple-500 px-4 py-2 text-sm text-white transition-colors hover:bg-purple-400"
       >
         Go Back
       </Link>
